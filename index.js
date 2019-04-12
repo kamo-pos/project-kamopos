5bda8123a940c60f41b6db327bc7cc24973cea0a
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-04-12 20:41:16 +0700

    add function showAllMenu

--- index.js
+++ index.js
@@ -16,5 +16,25 @@ const menuList = [
     name: "Sayur Asem",
     price: 13000,
     category: "menu nasi"
+  },
+  {
+    id: 4,
+    name: "Teh Manis",
+    price: 5000,
+    category: "minuman"
   }
 ];
+
+const foodName = menuList.map(item => item.name);
+const foodPrice = menuList.map(item => item.price);
+const foodCategory = menuList.map(item => item.category);
+
+const showAllMenu = () => {
+  const allMenu = (document.getElementById("menu-grid").innerHTML =
+    foodName + foodPrice + foodCategory);
+
+  console.log(allMenu);
+  return allMenu;
+};
+
+showAllMenu();
