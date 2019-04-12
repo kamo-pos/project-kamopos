8adfe70c6098fba580d952498b7d8455a64e8631
Sarah Julia Gushef <sarahgushef@gmail.com>
2019-04-12 20:05:01 +0700

    add html file, and menulist on js file

---
+++ index.js
@@ -0,0 +1,20 @@
+const menuList = [
+  {
+    id: 1,
+    name: "Nasi Goreng",
+    price: 10000,
+    category: "menu nasi"
+  },
+  {
+    id: 2,
+    name: "Sate Ayam",
+    price: 15000,
+    category: "menu daginng"
+  },
+  {
+    id: 3,
+    name: "Sayur Asem",
+    price: 13000,
+    category: "menu nasi"
+  }
+];
