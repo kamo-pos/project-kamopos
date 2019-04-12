4ccbfdfad79cb2e8d0c0c4ac422130f7a33e8105
nchristanto <christanto.nanda@gmail.com>
2019-04-12 22:01:50 +0700

    add function to helper.js

--- helper.js
+++ helper.js
@@ -7,9 +7,10 @@ let displayMenu = [
         food: [menuList.name],
         category: [menuList.category],
         quantity: 2, 
+        price: [menuList.price], 
         tax: 100000, 
         subtotal: [],
-        price: [], 
+        total: []
 
     }, 
     {
@@ -20,9 +21,11 @@ let displayMenu = [
         food: [menuList.name], 
         category: [menuList.category], 
         quantity: 3, 
+        price: [menuList.price],
         tax: 100000,
         subtotal: [],
-        price: []
+        total: []
+
     }, 
     {
         id: [menuList.id],
@@ -32,11 +35,27 @@ let displayMenu = [
         food: [menuList.name], 
         category: [menuList.category], 
         quantity: 1, 
+        price: [menuList.price],
         tax: 100000,
         subtotal: [],
-        price: []
+        total: []
+
     }
 ]
 
-const calculateTotal = displayMenu 
+
+const calculateTotal = () => {
+    let subTotal = (displayMenu.price * displayMenu.quantity);
+    let checkTotal = subTotal + displayMenu.tax;
+
+    const menuSubTotal = displayMenu.subTotal
+    const menuTotal = displayMenu.total
+
+    menuSubTotal.push(subTotal);
+    menuTotal.push(menuTotal)
+
+   return 
+}
+
+console.log(calculateTotal(displayMenu))
 
