952a289a8a60e0090941b01818bb4ee79fded1bb
Zein Hammad Marhabah <zeinmrhb@gmail.com>
2019-04-14 14:04:32 +0700

    Create new layout

---
+++ new-layout/index.js
@@ -0,0 +1,65 @@
+const mySiblings = [{
+    id: 1,
+    name: 'Aisyah',
+    status: 'Married',
+    numberOfChild: 4,
+    job: 'Teacher',
+    addres: 'Pakis Village, Semarang'
+  },
+  {
+    id: 2,
+    name: 'Fathullah',
+    status: 'Married',
+    numberOfChild: 3,
+    job: 'Teacher',
+    addres: 'Kebon Jeruk, Jakarta'
+  },
+  {
+    id: 3,
+    name: 'Khadijah',
+    status: 'Married',
+    numberOfChild: 2,
+    job: 'House wife',
+    addres: 'Kebayoran Baru, Jakarta'
+  },
+  {
+    id: 4,
+    name: 'Ahmad Balya',
+    status: 'Married',
+    numberOfChild: 2,
+    job: 'Teacher',
+    addres: 'Ciledug, Tangerang'
+  },
+  {
+    id: 5,
+    name: 'Halimah',
+    status: 'Married',
+    numberOfChild: 0,
+    job: 'Teacher',
+    addres: 'Cipadu, Tangerang'
+  },
+  {
+    id: 6,
+    name: 'Muhammad Alwi',
+    status: 'Married',
+    numberOfChild: 3,
+    job: 'Teacher',
+    addres: 'Kebon Jeruk, Jakarta'
+  },
+  {
+    id: 7,
+    name: 'Alsinah Mubarokah',
+    status: 'Married',
+    numberOfChild: 0,
+    job: 'Teacher',
+    addres: 'Pondok Aren, Tangerang'
+  },
+  {
+    id: 8,
+    name: 'Hamidah Nuroh',
+    status: 'Not Married',
+    numberOfChild: 0,
+    job: 'Student',
+    addres: 'Kebon Jeruk, Jakarta'
+  }
+]
